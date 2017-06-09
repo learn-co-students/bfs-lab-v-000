04d784f67f333d10a6afe792be69a597a7785e8b
zacscodingclub <zbaston@gmail.com>
2017-06-09 07:54:11 -0400

    WIP findAdjacent tests passing

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 function bfs(rootNode, vertices, edges){
+  rootNode.distance = 0;
+  let queue = [rootNode];
+  let filteredVertices = findAndRemoveVertex(vertex, vertices);
 
+  while(queue) {
+    const firstNode = queue.shift();
+    const adjacentVertices = findAdjacent(firstNode, vertices, edges);
+    adjacentVertices.forEach(vertex => {
+      markDistanceAndPredecessor(vertex);
+      queue.push(vertex);
+    })
+  }
+}
+
+function findAdjacent(vertexName, vertices, edges) {
+  const adjacentEdges = findEdges(vertexName, edges);
+  const adjacentNodeNames = [].concat(adjacentEdges[0], adjacentEdges[1]).filter(e => e !== vertexName);
+
+  return vertices.filter(v => {
+    if (adjacentNodeNames.includes(v.name)) { return v };
+  });
+}
+
+function findEdges(vertexName, edges) {
+  return edges.filter(edge => {
+    if (edge.includes(vertexName)) { return edge; }
+  });
+}
+
+function findAndRemoveVertex(vertex, vertices) {
+  return vertices.filter(v => {
+    if (v.name !== vertex.name) { return v };
+  })
 }
