dcd569319f6bd5fc2f289aeb73cb3b99bbc364ae
KerryAlsace <kerrydougherty@gmail.com>
2018-01-18 12:00:47 -0500

    save

--- index.js
+++ index.js
@@ -1,3 +1,134 @@
-function bfs(rootNode, vertices, edges){
+function bfs(rootNode, verticess, edgess) {
+    let rNodes = [];
+    let queue = [];
+    // if (verticess[0] === rootNode) {
+    //     rootNode.distance = 0;
+    // }
+
+    verticess[0].distance = 0;
+    queue.push(rootNode)
+
+    // rNodes.push(rootNode)
+
+    // let nodes = findAdjacent(rootNode, verticess, edgess)
+    // let mNodes = markDistanceAndPredecessor(rootNode, nodes)
+
+    // queue.push(mNodes[0])
+    // queue.push(mNodes[1])
+    console.log("here 1")
+    console.log(queue)
+    console.log(queue.length)
+
+    while (queue.length > 0) {
+        console.log("here 2")
+        stuff(rNodes, verticess, edgess, queue)
+        // let fNode = queue.shift
+        // rNodes.push(fNode)
+        // let nnodes = findAdjacent(fNode, verticess, edgess)
+        // let mmNodes = markDistanceAndPredecessor(fNode, nnodes)
+        // for (let i = 0; i < mmNodes.length; i++) {
+        //     if (mmNodes[i]) {
+        //         queue.push(mmNodes[i])
+        //         stuff(rNodes, verticess, edgess, queue)
+        //     }
+        // }
+    }
+
+    console.log("here 3")
+
+    console.log(rNodes)
+    return rNodes
+
+
+
+
+
+    // mNodes.map((n) => {
+    //     return bfs(n, verticess, edgess)
+    // })
+    // mNodes.forEach((n) => {
+    //     bfs(n, verticess, edgess)
+    // })
+
+    // for (let i = 0; i < mNodes.length; i++) {
+    //     bfs(mNodes[i], verticess, edgess)
+    //     return mNodes[i]
+    // }
+
 
 }
+
+function stuff(rNodes, verticess, edgess, queue) {
+    console.log("stuff")
+    console.log(queue)
+    let fNode = queue.shift
+    rNodes.push(fNode)
+    let nnodes = findAdjacent(fNode, verticess, edgess)
+    let mmNodes = markDistanceAndPredecessor(fNode, nnodes)
+    for (let i = 0; i < mmNodes.length; i++) {
+        queue.push(mmNodes[i])
+        stuff(rNodes, verticess, edgess, queue)
+    }
+}
+
+function findAdjacent(node, vertices, edges) {
+    let adjacentNodes = [];
+
+    for (let i = 0; i < edges.length; i++) {
+        let e = edges[i];
+        if (e[0] === node || e[0] === node.name) {
+            vertices.forEach((v) => {
+                if (v.name === e[1] && v.distance === null) {
+                    adjacentNodes.push(v)
+                }
+            })
+        } else if (e[1] === node || e[1] === node.name) {
+            vertices.forEach((v) => {
+                if (v.name === e[0] && v.distance === null) {
+                    adjacentNodes.push(v)
+                }
+            })
+        }
+    }
+
+    return adjacentNodes
+}
+
+function markDistanceAndPredecessor(node, adjacentNodes) {
+    adjacentNodes.forEach((n) => {
+        n.distance = node.distance + 1;
+        n.predecessor = node
+    })
+
+    return adjacentNodes
+}
+
+let edgess = [
+    ['14th&6th', '23rd&6th'],
+    ['23rd&6th', '34th&6th'],
+    ['34th&6th', '28th&Bwy'],
+    ['28th&Bwy', '23rd&Bwy'],
+    ['23rd&Bwy', '14th&Lex'],
+    ['14th&Lex', '23rd&Lex']
+]
+
+let verticess = [
+    { name: '34th&6th', distance: null, predecessor: null },
+    { name: '23rd&6th', distance: null, predecessor: null },
+    { name: '28th&Bwy', distance: null, predecessor: null },
+
+    { name: '14th&6th', distance: null, predecessor: null },
+    { name: '23rd&Bwy', distance: null, predecessor: null },
+    { name: '14th&Lex', distance: null, predecessor: null },
+    { name: '23rd&Lex', distance: null, predecessor: null }
+]
+
+// console.log(findAdjacentNodes('34th&6th', vertices, edges))
+// console.log("Should be")
+// console.log([{ name: '23rd&6th', distance: null, predecessor: null },
+// { name: '28th&Bwy', distance: null, predecessor: null }
+// ])
+
+bfs(verticess[0], verticess, edgess).map((vertex) => {
+    console.log(vertex.name)
+})
--- test/index-test.js
+++ test/index-test.js
@@ -3,40 +3,40 @@ var sinon = require('sinon');
 
 let edges;
 let vertices;
-beforeEach(function() {
-	edges = [
-		['14th&6th', '23rd&6th'],
-		['23rd&6th', '34th&6th'],
-		['34th&6th', '28th&Bwy'],
-		['28th&Bwy', '23rd&Bwy'],
-		['23rd&Bwy', '14th&Lex'],
-		['14th&Lex', '23rd&Lex']
-	]
+beforeEach(function () {
+  edges = [
+    ['14th&6th', '23rd&6th'],
+    ['23rd&6th', '34th&6th'],
+    ['34th&6th', '28th&Bwy'],
+    ['28th&Bwy', '23rd&Bwy'],
+    ['23rd&Bwy', '14th&Lex'],
+    ['14th&Lex', '23rd&Lex']
+  ]
 
-	vertices = [
-	  {name: '34th&6th', distance: null, predecessor: null},
-	  {name: '23rd&6th', distance: null, predecessor: null},
-		{name: '28th&Bwy', distance: null, predecessor: null},
+  vertices = [
+    { name: '34th&6th', distance: null, predecessor: null },
+    { name: '23rd&6th', distance: null, predecessor: null },
+    { name: '28th&Bwy', distance: null, predecessor: null },
 
-	  {name: '14th&6th', distance: null, predecessor: null},
-	  {name: '23rd&Bwy', distance: null, predecessor: null},
-	  {name: '14th&Lex', distance: null, predecessor: null},
-	  {name: '23rd&Lex', distance: null, predecessor: null}
-	]
+    { name: '14th&6th', distance: null, predecessor: null },
+    { name: '23rd&Bwy', distance: null, predecessor: null },
+    { name: '14th&Lex', distance: null, predecessor: null },
+    { name: '23rd&Lex', distance: null, predecessor: null }
+  ]
 })
 
-describe('#findAdjacentNodes', function() {
-  it("should return an array of adjacent nodes", function() {
-    expect(findAdjacent('34th&6th',  vertices, edges)).toEqual([{name: '23rd&6th', distance: null, predecessor: null},
-      {name: '28th&Bwy', distance: null, predecessor: null}
+describe('#findAdjacentNodes', function () {
+  it("should return an array of adjacent nodes", function () {
+    expect(findAdjacent('34th&6th', vertices, edges)).toEqual([{ name: '23rd&6th', distance: null, predecessor: null },
+    { name: '28th&Bwy', distance: null, predecessor: null }
     ])
   });
 
-  it("excludes discovered nodes", function() {
-		let thirtyFourthAndSixth = vertices[0]
-		thirtyFourthAndSixth.distance = 0
-    expect(findAdjacent('28th&Bwy',  vertices, edges)).toEqual([
-      {name: '23rd&Bwy', distance: null, predecessor: null}
+  it("excludes discovered nodes", function () {
+    let thirtyFourthAndSixth = vertices[0]
+    thirtyFourthAndSixth.distance = 0
+    expect(findAdjacent('28th&Bwy', vertices, edges)).toEqual([
+      { name: '23rd&Bwy', distance: null, predecessor: null }
     ])
     // but these nodes are not in the node list?
     // 23rd&Broadway
@@ -44,8 +44,8 @@ describe('#findAdjacentNodes', function() {
   })
 });
 
-describe('#markDistanceAndPredecessor', function() {
-  it("should return an array of adjacent nodes", function() {
+describe('#markDistanceAndPredecessor', function () {
+  it("should return an array of adjacent nodes", function () {
     let twentyThirdAndSixth = vertices[1]
     let twentyEighthAndBroadway = vertices[4]
     let thirtyFourthAndSixth = vertices[0]
@@ -60,9 +60,10 @@ describe('#markDistanceAndPredecessor', function() {
 });
 
 
-describe('#bfs', function() {
-	it("should return an array of nodes in the order they were visited", function() {
-		let startingNode = vertices[0]
-		expect(bfs(startingNode, vertices, edges).map(function(vertex){ return vertex.name; })).toEqual(['34th&6th', '23rd&6th', '28th&Bwy', '14th&6th', '23rd&Bwy', '14th&Lex', '23rd&Lex'])
-	})
+describe('#bfs', function () {
+  it("should return an array of nodes in the order they were visited", function () {
+    let startingNode = vertices[0]
+    console.log(bfs(startingNode, vertices, edges))
+    expect(bfs(startingNode, vertices, edges).map(function (vertex) { return vertex.name; })).toEqual(['34th&6th', '23rd&6th', '28th&Bwy', '14th&6th', '23rd&Bwy', '14th&Lex', '23rd&Lex'])
+  })
 })
