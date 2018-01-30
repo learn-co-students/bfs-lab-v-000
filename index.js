5275b8819c0fc1c39300912efd2de7a66e084c4c
Alexandra <github email address>
2018-01-30 12:35:49 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 function bfs(rootNode, vertices, edges){
+  rootNode.distance = 0
+  let queue = []
+  let disco = [rootNode]
+  let adjacentNodes
+  queue.push(rootNode)
+  while (queue.length > 0) {
+    let currentNode = queue.shift()
+    adjacentNodes = adjacentNodes = findAdjacent(currentNode.name, vertices, edges)
+      markDistanceAndPredecessor(currentNode, adjacentNodes)
+      queue = queue.concat(adjacentNodes)
+      disco = disco.concat(adjacentNodes)
+  }
+  return disco
+}
+
+function findAdjacent(node, vertices, edges){
+  const adjacentNodes = []
+  let matches
+  for (const edge of edges) {
+    if (edge.includes(node)) {
+      const target = edge.find(vertex =>(
+        vertex !== node
+      ))
+      const targetVertex = vertices.find(myVertex =>(
+        myVertex.name === target
+      ))
+        adjacentNodes.push(targetVertex)
+    }
+  }
+  return adjacentNodes.filter(myNode => (myNode.distance == null))
+}
+
+function markDistanceAndPredecessor(OGNode, adjacentNodes){
+  for (const adjacentNode of adjacentNodes) {
+    adjacentNode.predecessor = OGNode
+    adjacentNode.distance = OGNode.distance + 1
+  }
+}
+
+function addToQueue(){
 
 }
