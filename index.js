4ac0e1572aa24d1a42ad56ceaacd1a65063863e1
Adam Tropp <adt6261@gmail.com>
2018-02-26 06:34:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 function bfs(rootNode, vertices, edges){
 
 }
+
+function findAdjacent(node, vertices, edges) {
+  let adjacent = [];
+  let adjacent_names = [];
+  for (let i=0; i < edges.length; i++) {
+    if (edges[i].includes(node)) {
+      adjacent_names.push(edges[i].filter((vertex => vertex != node)))
+    }
+  }
+  debugger;
+  for (let i=0; i < adjacent_names.length; i++) {
+    adjacent.push(vertices.filter((vertex => vertex.name == adjacent_names[i] && vertex.distance == null)))
+  }
+  return adjacent
+}
+
+function markDistanceAndPredecessor(node, adjacentNodes) {
+  return node
+}
