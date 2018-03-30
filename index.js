cafc9fe4634c13f203b9ac153968627a869fe26a
github username <juliannne@gmail.com>
2018-03-29 23:49:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-function bfs(rootNode, vertices, edges){
+function bfs(startingNode, vertices, edges){
+  let queue = []
+  let nodesVisited = []
+  queue.push(startingNode)
+  while(queue.length !== 0) {
+    let firstNode = queue.shift()
+    if (!nodesVisited.includes(firstNode)) {
+      nodesVisited.push(firstNode)
+    }
+    let adjacentVertices = findAdjacent(firstNode.name, vertices, edges)
+      for (let i = 0; i < adjacentVertices.length; i++) {
+        markDistanceAndPredecessor(adjacentVertices[i], adjacentVertices)
+        queue.push(adjacentVertices[i])
+      }
+    }
+    return nodesVisited
+}
 
+function findAdjacent(node, vertices, edges) {
+  let adjacentVertices = []
+  let adjacentNames = []
+  for (let i = 0; i < edges.length; i++) {
+    if (edges[i].includes(node)) {
+      for (let j = 0; j < edges[i].length; j++) {
+        if (edges[i][j] != node) {
+          adjacentNames.push(edges[i][j])
+        }
+      }
+    }
+  }
+  for (let k = 0; k < vertices.length; k++) {
+    for (let l = 0; l < adjacentNames.length; l++) {
+      if (vertices[k].name == adjacentNames[l] && vertices[k].distance === null) {
+        adjacentVertices.push(vertices[k])
+      }
+    }
+  }
+  return adjacentVertices
+}
+
+function markDistanceAndPredecessor(node, adjacentNodes) {
+  for (let i = 0; i < adjacentNodes.length; i++) {
+    adjacentNodes[i].distance += 1
+    adjacentNodes[i].predecessor = node
+  }
+  return adjacentNodes
 }
