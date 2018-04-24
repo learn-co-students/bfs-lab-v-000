f452fb7ff9153691df36f986c8c94684634d8aff
Soumya Veer <veer.soumya@gmail.com>
2018-04-24 22:59:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-function bfs(rootNode, vertices, edges){
 
+function bfs(rootNode, vertices, edges){
+  rootNode.distance = 0;
+  let discoveredQueue = [rootNode];
+  let discoverOrder = [rootNode];
+
+  while(discoveredQueue.length != 0) {
+    let currentNode = discoveredQueue.shift();
+    let adjacentVertices = findAdjacent(currentNode.name, vertices, edges);
+    discoverOrder = discoverOrder.concat(adjacentVertices);
+    markDistanceAndPredecessor(currentNode, adjacentVertices)
+    discoveredQueue = discoveredQueue.concat(adjacentVertices)
+  }
+  return discoverOrder;
+}
+
+
+function findAdjacent(node, vertices, edges){
+  let adjacentNodes = [];
+  let adjacentVertices = [];
+  edges.forEach(edge => {
+    if(edge[0] === node){
+      adjacentNodes = [
+        ...adjacentNodes,
+        edge[1]
+      ]
+    }
+
+    if(edge[1] === node){
+      adjacentNodes = [
+        ...adjacentNodes,
+        edge[0]
+      ]
+    }
+  })
+
+  vertices.forEach(vertex => {
+    if(vertex.name === adjacentNodes[0] || vertex.name === adjacentNodes[1]) {
+      vertex.distance === null ?
+        adjacentVertices = [
+          ...adjacentVertices,
+          vertex
+        ]
+        :
+        adjacentVertices
+    }
+  })
+
+ return adjacentVertices
+}
+
+
+function markDistanceAndPredecessor(predecessor, adjacentNodes) {
+  adjacentNodes.map(adjacentNode => {
+    adjacentNode.distance = predecessor.distance + 1;
+    adjacentNode.predecessor = predecessor
+  })
 }
