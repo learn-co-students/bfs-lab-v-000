259dab4b9c074f0aee4885e1f2678f61364addd8
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-10-20 21:26:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 function bfs(rootNode, vertices, edges){
+  let queue = [];
+  let explored = [];
+  let adjacentVertices;
+  rootNode.distance = 0;
+  queue.push(rootNode);
+  while (queue.length !== 0) {
+    let firstNode = queue.shift();
+    adjacentVertices = findAdjacent(firstNode.name, vertices, edges);
+    markDistanceAndPredecessor(firstNode, adjacentVertices);
+    for (const vertex of adjacentVertices) {
+      queue.push(vertex);
+    }
+    explored.push(firstNode);
+  }
+  return explored;
+}
 
+function findAdjacent(vertexName, vertices, edges) {
+  let adjacentVertices = [];
+  for (const edge of edges) {
+    if (edge.includes(vertexName)) {
+      let adjacentVertexName = edge.filter(name => name !== vertexName)[0];
+      let adjacentVertex = vertices.filter(vertex => vertex.name === adjacentVertexName);
+      adjacentVertices.push(adjacentVertex[0]);
+    }
+  }
+  return adjacentVertices.filter(vertex => vertex.distance === null);
+}
+
+function markDistanceAndPredecessor(prevNode, adjacentNodes) {
+  for (const node of adjacentNodes) {
+    node.distance = prevNode.distance + 1;
+    node.predecessor = prevNode;
+  }
 }
