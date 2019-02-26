0b0c9359919f358bf7dcc5a54a7ac535cbe37ac4
VictoriaMeng <1ststarofthenight@gmail.com>
2019-02-26 14:10:59 -0800

    Complete findAdjacent() w/ .map & .filter Filter edges for edges that contain rootNode Retrieve adjacent vertex names w/ .map & .find Filter vertices to return full vertex objects w/ matching name keys

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ function bfs(rootNode, vertices, edges){
 }
 
 function findAdjacent(rootNode, vertices, edges) {
-  // Filter edges for vertex pairs containing rootNode
-  // Remove rootNode and flatten filtered array for adjacent vertices
-  // Iterate over vertices for objects where name key matches array elements
-  const names = edges.filter(pair => pair.includes(rootNode))
-  return names
+  const pairs = edges.filter(e => e.includes(rootNode))
+  let names = pairs.map(e => e.find(n => n != rootNode))
+  return vertices.filter(v => names.includes(v.name) && v.distance == null)
 }
