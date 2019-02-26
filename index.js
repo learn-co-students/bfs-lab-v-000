16b52dfe31aac16f85ec9cfbcddb44535b37b99f
VictoriaMeng <1ststarofthenight@gmail.com>
2019-02-26 13:41:29 -0800

    Begin findAdjacent() by defining names array Array of names defined by filtering edges arg for results w/ rootNode

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function bfs(rootNode, vertices, edges){
 
 }
+
+function findAdjacent(rootNode, vertices, edges) {
+  // Filter edges for vertex pairs containing rootNode
+  // Remove rootNode and flatten filtered array for adjacent vertices
+  // Iterate over vertices for objects where name key matches array elements
+  const names = edges.filter(pair => pair.includes(rootNode))
+  return names
+}
