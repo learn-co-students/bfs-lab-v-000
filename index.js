07e8fef3e812a3aab2c7b5898a022e8713d6598b
VictoriaMeng <1ststarofthenight@gmail.com>
2019-02-26 16:24:25 -0800

    Complete bfs() w/o recursion Initialize empty nodes array Initialize queue array w/ rootNode as only element Set rootNode.distance to 0 Set while loop that will run when queue has elements Shift rootNode out of queue, save as currentNode Use currentNode.name to find adjacentNodes Invoke markDistanceAndPredecessor() to set adjacentNodes properties Use for...in to loop over adjacentNodes and add to queue Push currentNode into nodes array Repeat loop until all nodes are in nodes array Return completed nodes array

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 function bfs(rootNode, vertices, edges){
+// Initialize empty nodes array
+// Initialize queue w/ rootNode only
+// Use findAdjacent() to return adjacentNodes
+// Use markDistanceAndPredeccesor to change adjacentNodes
+// Remove rootNode from queue, replace w/ first adjacentNode
+// Push former array in queue into nodes array
+// Repeat until all nodes were in queue
+// Return nodes array
 
+  const nodes = []
+  const queue = [rootNode]
+  rootNode.distance = 0
+  while (queue.length !== 0) {
+    let currentNode = queue.shift()
+    let adjacentNodes = findAdjacent(currentNode.name, vertices, edges)
+    markDistanceAndPredecessor(currentNode, adjacentNodes)
+    for (const node of adjacentNodes) {
+      queue.push(node)
+    }
+    nodes.push(currentNode)
+  }
+  return nodes
 }
 
 function findAdjacent(rootNode, vertices, edges) {
