ab4d57a4316610afd52717f4da8fc249e14b3d88
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-14 17:12:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-function bfs(rootNode, vertices, edges){
+function bfs(startNode, vertices, edges){
+  startNode.distance = 0;
+  let queue = [startNode];
+  let queueOrder = [startNode];
 
+  while(queue.length != 0) {
+    let currentNode = queue.shift();
+    let adjacentVertices = findAdjacent(currentNode.name, vertices, edges);
+    queueOrder = queueOrder.concat(adjacentVertices);
+    markDistanceAndPredecessor(currentNode, adjacentVertices)
+    queue = queue.concat(adjacentVertices)
+    }
+  return queueOrder
+}
+
+let findVertex = (vertexName, vertices) => {
+  return vertices.find(vertex => {
+    return vertex.name == vertexName;
+  });
+}
+
+let findAdjacent = (vertex, vertices, edges) => {
+  return edges.filter(edge => {
+    return edge.includes(vertex);
+  }).map(edge => {
+    return edge.filter(node => {
+      return node != vertex;
+    });
+  }).map(vertexName => {
+    return findVertex(vertexName, vertices);
+  }).filter(node => {
+    return node.distance == null;
+  });
+}
+
+let markDistanceAndPredecessor = (vertex, adjacentVertices)=> {
+  adjacentVertices.map(adjacentVertex => {
+    adjacentVertex.distance = vertex.distance + 1;
+    adjacentVertex.predecessor = vertex;
+  });
 }
