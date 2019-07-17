533c6a42597ae74e62e0d3038ef4f7f7d3f75a8a
blackbeltdiaries <github email address>
2019-07-17 10:26:21 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 function bfs(rootNode, vertices, edges){
+  rootNode.distance = 0;
 
+  let discovered = [rootNode];
+  let discoverOrder = [rootNode]
+
+  while(discovered.length != 0) {
+    let currentNode = discovered.shift();                                 // grab 1st element
+    let adjacentNodes = findAdjacent(currentNode.name, vertices, edges)   // retrive adjacent nodes
+
+    discoverOrder = discoverOrder.concat(adjacentNodes)
+    markDistanceAndPredecessor(currentNode, adjacentNodes)
+    discovered = discovered.concat(adjacentNodes)
+  }
+  return discoverOrder
+}
+
+function findAdjacent(nodeName, vertices, edges) {
+    return edges.filter(function(edge) {  // collects all edges with nodeName
+      return edge.includes(nodeName)
+    }).map(function(edge) {               // maps filtered edges
+      return edge.filter(function(node) { // collects all name of nodes from collected vertices and removes nodeName
+        return (node != nodeName)
+      })[0]
+    }).map(function(name) {               // uses mapped names to retrieve node
+      return findNode(name, vertices)
+    }).filter(function(node) {            // lastly filters nodes that are adjacent
+      return node.distance == null
+    })
+}
+
+function markDistanceAndPredecessor(predecessor, adjacentNodes) {
+  adjacentNodes.map(function(node) {
+    node.distance = predecessor.distance + 1;
+    node.predecessor = predecessor;
+  })
+}
+
+function findNode(nodeName, vertices) {   // retrieves vertix of nodeName
+  return vertices.find(function(vertex) {
+    return vertex.name == nodeName
+  })
 }
