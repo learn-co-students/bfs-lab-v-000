365bcf22a5bdf9a1e226144e54f68b4a8f6d9f6e
christianavdm <christianavdm@gmail.com>
2019-09-11 18:01:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
 function bfs(rootNode, vertices, edges){
 
 }
+
+
+/* rootNode = vertices[0]
+queue = []
+addVertexToQueue(rootNode)
+    // queue = [rootNode]
+while(!queue.length == 0) {
+    let firstNode = queue.shift()
+adjacentVertices = findAdjacent(firstNode)
+    for vertex in adjacentVertices {
+        markDistanceAndPredecessor(vertex)
+        addToQueue(vertex)
+    }
+} */
+
+
+function findAdjacent(verticeName, vertices, edges){
+    let adjacentNodes = []; 
+
+    for(let i = 0; i < edges.length; i++){
+        let currentEdge = edges[i];
+        let verticeA = currentEdge[0]; 
+        let verticeB = currentEdge[1]; 
+
+        if(verticeA == verticeName){
+            let adjacentVertixIndex = findVerticeIndexByName(verticeB, vertices); 
+            if (isVerticeUndiscovered(vertices[adjacentVertixIndex])) {
+                adjacentNodes.push(vertices[adjacentVertixIndex])
+            }
+        } else if(verticeB == verticeName){
+            let adjacentVertixIndex = findVerticeIndexByName(verticeA, vertices); 
+            if (isVerticeUndiscovered(vertices[adjacentVertixIndex])) {
+                adjacentNodes.push(vertices[adjacentVertixIndex])
+            }
+        }
+     }
+
+    return adjacentNodes; 
+}
+
+function findVerticeIndexByName(name, vertices) {
+    let index = 0
+    while(vertices[index].name != name){
+        index++
+    }
+    return index   
+}
+
+function isVerticeUndiscovered(vertix) {
+    if (vertix.distance != null) {
+        return false; 
+    } else {
+        return true; 
+    } 
+}
